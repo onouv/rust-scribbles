559244b307c406aeb1c11ff2db39a82543adf13e
olaf <onouv@t-online.de>
2025-03-30 17:02:54 +0200

    cleanup, create README

---
+++ README.md
@@ -0,0 +1,12 @@
+# Rust Scribbles
+
+
+A repo with a collection of various rust scribbles to serve as a personal notepad for Rust.
+
+All scribbles are available on a separate, non-merged branch:
+
+
+| Scribble      | Description                       | branch                           |
+|---------------|-----------------------------------|----------------------------------|
+| Tool FSM      | An fsm for the tool in the mes    | tool-fsm                         |
+| ping pong     | Two Actors playing ping pong      | ping-pong                        |
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,4 @@
 
-mod tool;
-use tool::*;
-
-// mod state_2;
-// use state_2::state_machine;
-
 fn main() {
-    let tool = Tool::new("1");
-    println!("{}", tool);
-
-    let tool = tool.handle(Signal::Start);
-    println!("{}", tool);
-    
-    let tool = tool.handle(Signal::LoadPart(Part {}));
-    println!("{}", tool);
-    let tool = tool.handle(tool::signal::Signal::LoadPart(Part {}));
-    println!("{}", tool);
-    let tool = tool.handle(tool::signal::Signal::LoadPart(Part {}));
-    println!("{}", tool);
-    // let tool = tool.stop
-
-    // state_machine(12);
+   
 }
