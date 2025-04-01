798c141073423726da703abe61d35d1e3ccc633d
olaf <onouv@t-online.de>
2025-04-01 10:17:49 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
 use actix::prelude::*;
 
-mod ping;
-use ping::{ PingActor, PingCfg };
-
-mod pong;
-use pong::*;
+mod early_checking;
+use early_checking::Service;
 
 #[actix::main]
 async fn main() {
-    let ping = PingActor::new().start();
-    let pong = PongActor::new(ping.clone().recipient()).start();
-
-    _ = ping.send(PingCfg { pong: pong.clone().recipient() }).await;
+    
+    let service = Service::new().await;
+    _ = service.do_me_a_service().await;
 
     tokio::signal::ctrl_c().await.unwrap();
 
